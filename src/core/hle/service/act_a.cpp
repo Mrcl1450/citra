c30208bc34f78461979dd4ded1e544ff44c7fa04
András Domonkos <katisx1@citromail.hu>
2016-05-16 14:04:14 +0200

    Fix wrong header

--- src/core/hle/service/act_a.cpp
+++ src/core/hle/service/act_a.cpp
@@ -2,7 +2,7 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#include "core/hle/service/act_u.h"
+#include "core/hle/service/act_a.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Namespace ACT_A
